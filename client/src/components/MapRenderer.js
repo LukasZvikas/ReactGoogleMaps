c8ac213aea4a81103b44eea8ba589f1762a766eb
LukasZvikas <lzvikas1@gmail.com>
2018-05-01 01:13:06 -0400

    added routing

---
+++ client/src/components/MapRenderer.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Map from "./Map";
+
+class MapRenderer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+      origin: {lat: 40.6976684, lng: -74.260552},
+      destination: {lat: 42.6976684, lng: -73.260552 }
+    };
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <Map
+        containerElement={
+          <div
+            style={{
+              position: "absolute",
+              width: "35%",
+              height: "80%"
+            }}
+          />
+        }
+        mapElement={
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0
+            }}
+          />
+        }
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        markers={{ lat: 40.6976684, lng: -74.260552 }}
+        origin={this.state.origin} 
+        destination={this.state.destination}
+      />
+    );
+  }
+}
+
+export default MapRenderer;
---
+++ client/src/components/RouteList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RouteList extends Component {
+  render() {
+    return <div>This is RouteList</div>;
+  }
+}
+
+export default RouteList;
